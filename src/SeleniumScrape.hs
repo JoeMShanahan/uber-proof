665ee8b79c77c1eb12aec375abc5dc5dcece1e9f
JoeMShanahan <joe.s.kasc@gmail.com>
2017-09-03 04:18:49 +0100

    Start up a WD sesssion

--- src/SeleniumScrape.hs
+++ src/SeleniumScrape.hs
@@ -1,5 +1,22 @@
 module SeleniumScrape
-  (
+  ( getTrips
   ) where
 
-import Protolude
+import Uberlude
+import Vision.Image
+import Types.UberTrip
+import Test.WebDriver
+
+getTrips :: String -> Int -> IO [UberTrip]
+getTrips host port = runSession (chromeConfig host port) $ do
+  openPage "http://google.com"
+  liftIO $ threadDelay 10000000
+  undefined
+
+chromeConfig :: String -> Int -> WDConfig
+chromeConfig host port = config
+  { wdHost = host
+  , wdPort = port
+  }
+  where
+  config = useBrowser chrome defaultConfig

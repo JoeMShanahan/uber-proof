e37ce8ae4f0c188595e042190cd5b84030514eb3
JoeMShanahan <joe.s.kasc@gmail.com>
2017-09-03 04:23:43 +0100

    Move bank card types to expenses

--- src/Types/Expenses.hs
+++ src/Types/Expenses.hs
@@ -1,5 +1,19 @@
 module Types.Expenses
-  (
+  ( BankCard
+  , CardType (..)
+  , makeCard
   ) where
 
 import Uberlude
+
+data BankCard = Card CardType Int
+  deriving (Eq, Show)
+
+data CardType = Visa | MasterCard
+  deriving (Eq, Show)
+
+makeCard :: CardType -> Int -> Maybe BankCard
+makeCard cardType last4Digits
+  | last4Digits < 0    = Nothing
+  | last4Digits > 9999 = Nothing
+  | otherwise          = Just $ Card cardType last4Digits
--- src/Types/UberTrip.hs
+++ src/Types/UberTrip.hs
@@ -1,25 +1,10 @@
 module Types.UberTrip
   ( UberTrip (..)
-
-  , BankCard
-  , CardType (..)
-  , makeCard
   ) where
 
 import Data.Time
+import Types.Expenses
 import Uberlude
-
-data BankCard = Card CardType Int
-  deriving (Eq, Show)
-
-data CardType = Visa | MasterCard
-  deriving (Eq, Show)
-
-makeCard :: CardType -> Int -> Maybe BankCard
-makeCard cardType last4Digits
-  | last4Digits < 0    = Nothing
-  | last4Digits > 9999 = Nothing
-  | otherwise          = Just $ Card cardType last4Digits
 
 data UberTrip = UberTrip
   { uberScreenshot :: ByteString

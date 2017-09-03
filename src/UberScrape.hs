83f7747f12c12973902c8699ee54e8dd4c251e94
JoeMShanahan <joe.s.kasc@gmail.com>
2017-09-03 04:45:10 +0100

    Remove 'undefined'

--- src/UberScrape.hs
+++ src/UberScrape.hs
@@ -11,7 +11,7 @@ getTrips :: String -> Maybe Int -> IO [UberTrip]
 getTrips host port = runSession (chromeConfig host port) $ do
   openPage "http://google.com"
   liftIO $ threadDelay 10000000
-  undefined
+  return []
 
 chromeConfig :: String -> Maybe Int -> WDConfig
 chromeConfig host port = addPort port $ 

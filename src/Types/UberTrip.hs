e5ff9c43a58f10fddcb12a1a4c0c500e9ac7b959
JoeMShanahan <joe.s.kasc@gmail.com>
2017-09-03 04:20:53 +0100

    Uber trip types

--- src/Types/UberTrip.hs
+++ src/Types/UberTrip.hs
@@ -1,5 +1,31 @@
 module Types.UberTrip
-  (
+  ( UberTrip (..)
+
+  , BankCard
+  , CardType (..)
+  , makeCard
   ) where
 
-import Protolude
+import Data.Time
+import Uberlude
+
+data BankCard = Card CardType Int
+  deriving (Eq, Show)
+
+data CardType = Visa | MasterCard
+  deriving (Eq, Show)
+
+makeCard :: CardType -> Int -> Maybe BankCard
+makeCard cardType last4Digits
+  | last4Digits < 0    = Nothing
+  | last4Digits > 9999 = Nothing
+  | otherwise          = Just $ Card cardType last4Digits
+
+data UberTrip = UberTrip
+  { uberScreenshot :: ByteString
+  , uberStartTime  :: UTCTime
+  , uberStartLoc   :: Text
+  , uberEndLoc     :: Text
+  , uberCost       :: Int
+  , userCard       :: BankCard
+  } deriving (Eq, Show)

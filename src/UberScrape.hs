d9a51e8be822d19fbc63dab8e89fcba2729a53c7
JoeMShanahan <joe.s.kasc@gmail.com>
2017-10-02 00:47:40 +0100

    Perform login

--- src/UberScrape.hs
+++ src/UberScrape.hs
@@ -3,13 +3,15 @@ module UberScrape
   ) where
 
 import Uberlude
-import Vision.Image
 import Types.Uber
 import Test.WebDriver
+import Test.WebDriver.Commands.Wait (waitUntil, onTimeout, unexpected)
+import Data.Text (toLower)
 
 getTrips :: String -> Maybe Int -> Username -> Password -> IO [UberTrip]
-getTrips host port user pass = runSession (chromeConfig host port) $ do
+getTrips host port user pwd = runSession (chromeConfig host port) $ do
   openPage uberPage
+  performUberLogin user pwd
   return []
 
 chromeConfig :: String -> Maybe Int -> WDConfig
@@ -20,7 +22,63 @@ chromeConfig host port = addPort $ config { wdHost = host }
 
 performUberLogin :: Username -> Password -> WD ()
 performUberLogin (Username user) (Password pwd) = do
-  undefined
+  putText "Attempting login"
+  
+  userInput <- findElem $ ById "useridInput"
+  sendKeys user userInput
+  findAndClickNext
+
+  passInput <- patiently $ findElem $ ById "password"
+  sendKeys pwd passInput
+  findAndClickNext
+
+  patiently loggedInOr2FA
+
+  putText "Login successful"
+
+  where
+  findAndClickNext = findAndClick "Next"
+  findAndClick t = do
+    es <- findElems (containsTextSelector t)
+    next <- filterM isDisplayed es
+    case next of
+      [nxt] -> click nxt
+      _     -> unexpected $ unpack $
+        "Found " <> show (length es) <> " " <> t <> " buttons"
+
+  loggedInOr2FA = do
+    login <- tryWD loginSuccess
+    case login of
+      Success _ -> return ()
+      Failure   -> do
+        mfaInput <- findElem $ ById "verificationCode"
+        putText "It looks like Uber is wanting a 2FA code, please enter it:"
+        code <- liftIO getLine
+        sendKeys code mfaInput
+        findAndClick "Verify"
+        patiently loginSuccess
+
+
+loginSuccess :: WD ()
+loginSuccess = void $ findElem $ containsTextSelector "MY TRIPS"
 
 uberPage :: String
 uberPage = "https://riders.uber.com"
+
+containsTextSelector :: Text -> Selector
+containsTextSelector t = ByXPath $
+     "//*[text()[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'"
+  <> toLower t
+  <> "')]]"
+
+patiently :: WD a -> WD a
+patiently = waitUntil waitTime
+
+waitTime :: Double
+waitTime = 60
+
+data WDSuccess a = Success a | Failure
+  deriving (Eq, Show)
+
+tryWD :: WD a -> WD (WDSuccess a)
+tryWD go = waitUntil 0 (go >>= return . Success) `onTimeout ` return Failure
--- src/Uberlude.hs
+++ src/Uberlude.hs
@@ -2,6 +2,8 @@ module Uberlude
   ( module Reexported
   ) where
 
-import Protolude as Reexported
+import Protolude as Reexported hiding (Selector)
 import Prelude   as Reexported (String) 
 import Data.Text as Reexported (unpack, pack)
+import Control.Monad as Reexported (fail)
+import Control.Exception.Base as Reexported (throw)
